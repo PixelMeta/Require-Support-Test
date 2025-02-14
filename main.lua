4c0996293808a7d4b543b41315810e3f3e218d37
PixelMeta <scripter.meta@gmail.com>
2025-02-14 14:40:01 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,11 +1,4 @@
 local executor = getgenv().identifyexecutor and getgenv().identifyexecutor() or "RobloxClientApp"
-local function notify(message)
-	game.StarterGui:SetCore("SendNotification", {
-		Title = "Executor Check";
-		Text = message;
-		Duration = 5;
-	})
-end
 
 if pcall(function() return require end) then
 	game.StarterGui:SetCore("SendNotification", {
