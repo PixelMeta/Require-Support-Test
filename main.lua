ee889cd9f1e4e110d131d8d5e9d21b38189837b4
PixelMeta <scripter.meta@gmail.com>
2025-02-14 13:12:21 +0100

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,23 @@
+local StarterGui = game:GetService("StarterGui")
+
+local checkedmodules = {}
+for _, module in ipairs(game:GetDescendants()) do
+    if module:IsA("ModuleScript") then
+        table.insert(checkedmodules, module)
+    end
+end
+
+if #modules == 0 then
+    StarterGui:SetCore("SendNotification", {Title = "Uh oh..", Text = "Join another game, this one doesnt have any modules"})
+    return
+end
+
+local rm = modules[math.random(1, #modules)]
+
+local success, result = pcall(require, rm)
+if success then
+    StarterGui:SetCore("SendNotification", {Title = "Require Support", Text = "Your executor has require Support!"})
+else
+    StarterGui:SetCore("SendNotification", {Title = "Require Support", Text = "Your executor has no require support."})
+  StarterGui:SetCore("SendNotification", {Title = "Require Support", Text = "Make sure to run it at least 3 times. If even one says 'Require Support' then it means there is support."})
+end
