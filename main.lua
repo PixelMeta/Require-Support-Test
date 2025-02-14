2674cb4dd3260614ab300cc6bd43093739a50153
PixelMeta <scripter.meta@gmail.com>
2025-02-14 13:20:55 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -7,17 +7,17 @@ for _, module in ipairs(game:GetDescendants()) do
     end
 end
 
-if #modules == 0 then
-    StarterGui:SetCore("SendNotification", {Title = "Uh oh..", Text = "Join another game, this one doesnt have any modules"})
+if #checkedmodules == 0 then
+    StarterGui:SetCore("SendNotification", {Title = "Uh oh..", Text = "Join another game, this one doesn't have any modules"})
     return
 end
 
-local rm = modules[math.random(1, #modules)]
+local rm = checkedmodules[math.random(1, #checkedmodules)]
 
 local success, result = pcall(require, rm)
 if success then
-    StarterGui:SetCore("SendNotification", {Title = "Require Support", Text = "Your executor has require Support!"})
+    StarterGui:SetCore("SendNotification", {Title = "Require Support", Text = "Your executor has require support!"})
 else
     StarterGui:SetCore("SendNotification", {Title = "Require Support", Text = "Your executor has no require support."})
-  StarterGui:SetCore("SendNotification", {Title = "Advice", Text = "Make sure to run it at least 3 times. If even one says 'Require Support' then it means there is support."})
+    StarterGui:SetCore("SendNotification", {Title = "Advice", Text = "Make sure to run it at least 3 times. If even one says 'Require Support' then it means there is support."})
 end
